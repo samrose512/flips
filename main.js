c79ab06b57ed781e288400dd83696fa28bf12338
Malachy Allen <samfujisakidev@gmail.com>
2020-07-19 21:27:52 +0100

    added some placeholder messageboxes, a placeholder icon and edited the CSS (not sure whether to change this later, probably will)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const electron = require('electron');
+const { dialog, app, Menu } = require('electron');
 
 function createWindow() {
   let window = new electron.BrowserWindow({
@@ -6,11 +7,39 @@ function createWindow() {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    icon: './icon.png'
   });
 
+  let menu = Menu.buildFromTemplate([
+    {
+      "label": "File",
+      "submenu": [
+        {"label": "Open...",
+      click() {
+        //window.webContents.send('open-file');
+        dialog.showMessageBox(null, { type: "info", buttons: ["OK"], title: "Open", message: "Open file placeholder", icon: "./icon.png"} );
+      }},
+        {"label": "Close",
+      click() {
+        window.close();
+      }}
+      ]
+    },
+    {
+      "label": "Help",
+      "submenu": [
+        {"label": "About",
+      click() {
+        //window.webContents.send('show-about');
+        dialog.showMessageBox(null, { type: "info", buttons: ["OK"], title: "About", message: "About program placeholder", icon: "./icon.png"} );
+      }}
+      ]
+    }
+  ]);
+  Menu.setApplicationMenu(menu);
+
   window.loadFile('index.html');
-  window.setMenu(null);
 }
 
 electron.app.on('ready', createWindow);
