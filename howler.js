4dbd478a3577bad29c1e1b23296e8feb67db35e4
Lucas Hernán Tarche <lucastarche@gmail.com>
2020-09-25 14:11:51 -0300

    Icons, Local files and stuff
    TODO: Toggle the button icon depending on the state

--- howler.js
+++ howler.js
@@ -1,28 +1,42 @@
 const howler = require('howler');
 
-let song = new Howl({
-    src: ['song.ogg']
-});
+let song;
 
+let loaded = false;
 let playing = false;
 
 function toggleSong() {
+    if (!loaded) return;
     playing = !playing;
     if (playing) song.play();
     else song.pause();
 }
 
 function stopSong() {
+    if (!loaded) return;
     playing = false;
     song.stop();
 }
 
+function loadSong() {
+    loaded = true;
+    const songFile = document.getElementById("songFile").files[0];
+    const src = URL.createObjectURL(songFile);
+    if (playing) stopSong();
+    song = new Howl({
+        src: [src],
+        ext: ['ogg', 'mp3'],
+        html5: true
+    });
+}
+
 setInterval(
     () => {
+        if (!loaded || !playing) return;
         const progressBar = document.getElementById("songDuration");
         const current = song.seek() * 100;
         const duration = song.duration();
-        const percentage = current / duration;
+        const percentage = Math.max(current / duration, 1);
         progressBar.value = percentage;
     }, 100
 );
--- main.js
+++ main.js
@@ -2,44 +2,85 @@ const electron = require('electron');
 const { dialog, app, Menu } = require('electron');
 
 function createWindow() {
-  let window = new electron.BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    },
-    icon: './icon.png'
-  });
+    let window = new electron.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        icon: './Images/icon.png'
+    });
 
-  let menu = Menu.buildFromTemplate([
-    {
-      "label": "File",
-      "submenu": [
-        {"label": "Open...",
-      click() {
-        //window.webContents.send('open-file');
-        dialog.showMessageBox(null, { type: "info", buttons: ["OK"], title: "Open", message: "Open file placeholder", icon: "./icon.png"} );
-      }},
-        {"label": "Close",
-      click() {
-        window.close();
-      }}
-      ]
-    },
-    {
-      "label": "Help",
-      "submenu": [
-        {"label": "About",
-      click() {
-        //window.webContents.send('show-about');
-        dialog.showMessageBox(null, { type: "info", buttons: ["OK"], title: "About", message: "About program placeholder", icon: "./icon.png"} );
-      }}
-      ]
-    }
-  ]);
-  Menu.setApplicationMenu(menu);
+    let menu = Menu.buildFromTemplate([
+        {
+            "label": "File",
+            "submenu": [
+                {
+                    "label": "Open...",
+                    click() {
+                        //window.webContents.send('open-file');
+                        dialog.showMessageBox(
+                            null, 
+                            { 
+                                type: "info", 
+                                buttons: ["OK"], 
+                                title: "Open", 
+                                message: "Open file placeholder", 
+                                icon: "./Images/icon.png"
+                            }
+                        );
+                    }
+                },
+                {
+                    "label": "Close",
+                    click() {
+                        window.close();
+                    }
+                }
+            ]
+        },
+        {
+            "label": "Debug",
+            "submenu": [
+                {
+                    "label": "DevTools",
+                    click() {
+                        window.webContents.toggleDevTools();
+                    }
+                },
+                {
+                    "label": "Reload",
+                    click() {
+                        window.reload();
+                    }
+                }
+            ]
+        },
+        {
+            "label": "Help",
+            "submenu": [
+                {
+                    "label": "About",
+                    click() {
+                        //window.webContents.send('show-about');
+                        dialog.showMessageBox(
+                            null, 
+                            { 
+                                type: "info", 
+                                buttons: ["OK"], 
+                                title: "About", 
+                                message: "About program placeholder", 
+                                icon: "./Images/icon.png"
+                            } 
+                        );
+                    }
+                }
+            ]
+        }
+    ]);
+    Menu.setApplicationMenu(menu);
 
-  window.loadFile('index.html');
+    window.loadFile('index.html');
 }
 
 electron.app.on('ready', createWindow);
